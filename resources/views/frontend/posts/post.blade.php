4adbe488614e3ddaddce8ad0e01c80957a4a5797
Ramesh Mhetre <ram@tradebyte.biz>
2019-01-10 09:03:28 +0100

    Post details UI
    Signed-off-by: Ramesh Mhetre <ram@tradebyte.biz>

--- resources/views/frontend/posts/post.blade.php
+++ resources/views/frontend/posts/post.blade.php
@@ -2,37 +2,32 @@
 
 @section('content')
 
-            <div class="columns is-centered">
+        <div class="columns is-centered">
 
-                <div class="column is-12">
+            <div class="column is-12">
 
-                    <div class="content">
+                <div class="content">
 
-                        <p class="short-post">
-                            <a href="#"><h2 class="title is-3">{{$post->title}}</h2></a>
-                            <h4 class="subtitle is-5">{{$post->publish_date->diffForHumans()}}</h4>
+                        <a href="#"><h2 class="title is-3">{{$post->title}}</h2></a>
+                        <h4 class="subtitle is-5">{{$post->publish_date->diffForHumans()}}</h4>
 
-                            <div>
-                                {!! $post->body !!}
-                            </div>
-
-                            <div class="tags">
-                                @foreach($post->tags as $tag)
-                                    <span class="tag is-primary is-large">{{$tag->name}}</span>
-                                @endforeach
-                            </div>
-
+                        <div>
+                            {!! $post->body !!}
                         </div>
 
+                        <div class="tags">
+                            @foreach($post->tags as $tag)
+                                <span class="tag is-primary is-large">{{$tag->name}}</span>
+                            @endforeach
+                        </div>
 
                     </div>
-                </div>
 
+
+                </div>
             </div>
 
-
-
-
+        </div>
 
 
 @endsection
