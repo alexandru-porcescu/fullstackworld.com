d4b2c94f39fde80e92eb6ddf2e3507aa17617305
Ramesh Mhetre <mhetreramesh@gmail.com>
2019-03-07 00:04:56 +0100

    Test command added
    Signed-off-by: Ramesh Mhetre <mhetreramesh@gmail.com>

---
+++ app/Console/Commands/TestLog.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class TestLog extends Command
+{
+    protected $signature = 'test:log';
+
+    protected $description = 'Test Log';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        Log::alert('Sent alert message on '. date('yyyy-mm-dd h:i:s'));
+
+        $this->info('Sync successfully finished!');
+    }
+}
