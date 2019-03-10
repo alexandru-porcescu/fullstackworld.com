b8ee1d382b44f258a4243b25fd28b66ad98ea625
Ramesh Mhetre <mhetreramesh@gmail.com>
2019-03-10 15:44:40 +0100

    Added footer link
    Signed-off-by: Ramesh Mhetre <mhetreramesh@gmail.com>

--- resources/views/frontend/layouts/partials/_footer.blade.php
+++ resources/views/frontend/layouts/partials/_footer.blade.php
@@ -2,23 +2,20 @@
     <div class="container">
 
         <div class="content has-text-centered">
-            <a class="has-text-primary" title="{{ route('about') }}" href="{{ route('about') }}">About</a>
-
+            <a class="has-text-primary" title="{{ route('about') }}" href="{{ route('about') }}">About</a> |
+            <a class="has-text-primary" title="{{ route('home') }}" href="{{ route('home') }}">Blog</a> |
+            <a class="has-text-primary" title="{{ route('journal') }}" href="{{ route('journal') }}">Journal</a>
         </div>
 
 
 
-        <div class="content has-text-centered">
-
-
+        <!--div class="content has-text-centered">
             <a target="_blank" href="https://github.com/mhetreramesh" title="Github"
                class="button is-small is-light navbar-icon"><i class="fab fa-github"></i></a>
 
             <a target="_blank" href="https://twitter.com/mhetreramesh" title="Twitter"
                class="button is-small is-light navbar-icon"><i class="fab fa-twitter"></i></a>
-
-        </div>
-
+        </div-->
 
 
         <div class="content has-text-centered">
@@ -33,3 +30,4 @@
     </div>
 
 </footer>
+
