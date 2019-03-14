50908fecd0f24a1900b062374b7e9233db7f9200
Ramesh Mhetre <mhetreramesh@gmail.com>
2019-03-14 21:59:28 +0100

    UI changes
    Signed-off-by: Ramesh Mhetre <mhetreramesh@gmail.com>

--- resources/views/frontend/home/post-card.blade.php
+++ resources/views/frontend/home/post-card.blade.php
@@ -1,24 +1,24 @@
 <div class="column is-one-third">
     <div class="short-post-card">
-        <div class="card">
-            <a href="{{route('post', $post->slug)}}">
+        <a href="{{route('post', $post->slug)}}">
+            <div class="card">
                 <div class="card-image">
                     <figure class="image is-2by1">
                     <img src="{{$post->featured_image ? $post->featured_image : asset('img/placeholder.png')}}" alt="{{$post->title}}">
                     </figure>
                 </div>
-            </a>
-            <div class="card-content">
-                <div class="media">
-                
-                    <div class="media-content">
-                        <h3 class="title is-4"><a href="{{route('post', $post->slug)}}">{{$post->title}}</a></h3>
-                        <p class="subtitle is-5"><time datetime="{{$post->publish_date}}"> {{$post->publish_date->format('M d, Y')}}</time></p>
-                    </div>
-                </div>
+                <div class="card-content">
+                    <div class="media">
 
+                        <div class="media-content">
+                            <h3 class="title is-4">{{$post->title}}</h3>
+                            <p class="subtitle is-5"><time datetime="{{$post->publish_date}}"> {{$post->publish_date->format('M d, Y')}}</time></p>
+                        </div>
+                    </div>
+
+                </div>
             </div>
-        </div>
-        
+        </a>
+
     </div>
 </div>
