ee1d16e2b99a6c412e20e0eb95c723bd93396f47
Ramesh Mhetre <mhetreramesh@gmail.com>
2019-04-14 22:11:11 +0200

    One signal added
    Signed-off-by: Ramesh Mhetre <mhetreramesh@gmail.com>

--- resources/views/frontend/layouts/partials/_scripts.blade.php
+++ resources/views/frontend/layouts/partials/_scripts.blade.php
@@ -10,6 +10,17 @@
     })();
 
 </script>
+
+<link rel="manifest" href="/manifest.json" />
+<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+<script>
+    var OneSignal = window.OneSignal || [];
+    OneSignal.push(function() {
+        OneSignal.init({
+            appId: "761db262-dc9b-4165-8e3d-8be8e183e2c3",
+        });
+    });
+</script>
 @include('bulma::notifications')
 
 @yield('scripts')
