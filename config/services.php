8bd25fae5d7b0afe57a13a37bec2d637045e993d
Ramesh Mhetre <ram@tradebyte.biz>
2019-04-23 10:54:25 +0200

    Added config
    Signed-off-by: Ramesh Mhetre <ram@tradebyte.biz>

--- config/services.php
+++ config/services.php
@@ -28,6 +28,9 @@ return [
 
     'sparkpost' => [
         'secret' => env('SPARKPOST_SECRET'),
+        'options' => [
+            'endpoint' => env('SPARKPOST_ENDPOINT','https://api.eu.sparkpost.com/api/v1')
+        ]
     ],
 
     'stripe' => [
