67b90dcf454d329c52f52b09c02537ba19542295
Ramesh Mhetre <ram@tradebyte.biz>
2019-05-02 11:01:26 +0200

    Sitemap fixed
    Signed-off-by: Ramesh Mhetre <ram@tradebyte.biz>

--- app/Console/Commands/CreateSitemap.php
+++ app/Console/Commands/CreateSitemap.php
@@ -39,7 +39,7 @@ class CreateSitemap extends Command
         $posts = WinkPost::all();
 
         foreach ($posts as $post) {
-            $sitemap->add('post/'.URL::to($post->slug), Carbon::now(), '0.9', 'weekly');
+            $sitemap->add(URL::to('post/'.$post->slug), Carbon::now(), '0.9', 'weekly');
         }
 
         $sitemap->store('xml', 'sitemap');
