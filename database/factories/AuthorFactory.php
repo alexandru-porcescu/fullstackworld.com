7b40780737cf33ff02ccc4e6e741ecc51e75565c
Ramesh Mhetre <mhetreramesh@gmail.com>
2019-08-04 10:07:53 +0200

    Author model test
    Signed-off-by: Ramesh Mhetre <mhetreramesh@gmail.com>

--- database/factories/AuthorFactory.php
+++ database/factories/AuthorFactory.php
@@ -2,24 +2,13 @@
 
 use Faker\Generator as Faker;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
-
 $factory->define(\Wink\WinkAuthor::class, function (Faker $faker) {
     return [
         'id' => (string) \Illuminate\Support\Str::uuid(),
-        'name' => 'Regina Phalange',
-        'slug' => 'regina-phalange',
-        'bio' => 'This is me.',
-        'email' => 'admin@mail.com',
-        'password' => \Illuminate\Support\Facades\Hash::make($password = str_random()),
+        'name' => $faker->name,
+        'slug' => $faker->slug,
+        'bio' => $faker->text,
+        'email' => $faker->email,
+        'password' => \Illuminate\Support\Facades\Hash::make($faker->password),
     ];
 });
---
+++ tests/MigrateFreshOnce.php
@@ -0,0 +1,20 @@
+<?php
+namespace Tests;
+
+use Illuminate\Support\Facades\Artisan;
+
+trait MigrateFreshOnce
+{
+    protected static $setUpHasRunOnce = false;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        if (! static::$setUpHasRunOnce)
+        {
+            Artisan::call('migrate:fresh');
+            static::$setUpHasRunOnce = true;
+        }
+    }
+}
