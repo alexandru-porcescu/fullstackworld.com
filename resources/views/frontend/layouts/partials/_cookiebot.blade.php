ebed520fd0bd87d0f418e17958af58ffbf9ea06b
Ramesh Mhetre <ram@tradebyte.biz>
2019-08-26 16:01:37 +0200

    Cookie bar fix

--- resources/views/frontend/layouts/partials/_cookiebot.blade.php
+++ resources/views/frontend/layouts/partials/_cookiebot.blade.php
@@ -1,4 +1,4 @@
-@if(app()->environment('production') && config('fullstackworld.show_cookiebar') && empty($blockAdsense))
+@if(app()->environment('production') && config('fullstackworld.show_cookiebar'))
 
     <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="ace37c31-ca73-4209-b28c-80c67d826e28" type="text/javascript" async></script>
 
