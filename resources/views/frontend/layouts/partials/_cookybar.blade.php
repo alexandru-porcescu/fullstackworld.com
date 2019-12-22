313efafb4f1e7b1c9224297b72c6b53aca59ce8c
Ramesh Mhetre <mhetreramesh@gmail.com>
2019-12-22 19:23:56 +0100

    Cookybar website source has been changed
    Signed-off-by: Ramesh Mhetre <mhetreramesh@gmail.com>

--- resources/views/frontend/layouts/partials/_cookybar.blade.php
+++ resources/views/frontend/layouts/partials/_cookybar.blade.php
@@ -1,5 +1,5 @@
 @if(app()->environment('production') && config('fullstackworld.show_cookybar'))
 
-    <script id="cookybar_script" src="https://cookybar.com/js/website/init.js" uuid="dabdc90b-2652-4550-b0ca-f60cae64b06f" type="text/javascript" async></script>
+    <script id="cookybar_script" src="https://cookybar.com/js/website/init.js" uuid="325da595-fd67-4fbb-89aa-f4b7a2759185" type="text/javascript" async></script>
 
 @endif
